e3b7db8976827eda3a1f97e523d74d9fd4bcdf7d
siehun <126447514+siehun@users.noreply.github.com>
2024-09-16 16:06:25 +0800

    submit 007 009

---
+++ src/main/java/day009/hw1.java
@@ -0,0 +1,46 @@
+package day009;
+
+import java.util.List;
+
+public class hw1 {
+    class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    class DoubleListNode {
+        public int val;
+        public DoubleListNode last;
+        public DoubleListNode next;
+        public DoubleListNode(int v) {
+            val = v;
+        }
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+    public DoubleListNode reverseDoubleList(DoubleListNode head) {
+        DoubleListNode pre = null;
+        DoubleListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.last = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+}
