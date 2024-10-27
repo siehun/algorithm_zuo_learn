767421f59aa82d62617f889d48af10c05eb72fe1
siehun <126447514+siehun@users.noreply.github.com>
2024-10-27 17:23:41 +0800

    按层序列化

--- src/main/java/day036/hw6.java
+++ src/main/java/day036/hw6.java
@@ -1,4 +1,7 @@
 package day036;
+
+import java.time.chrono.MinguoDate;
+
 // 二叉树按层序列化和反序列化
 // 测试链接 : https://leetcode.cn/problems/serialize-and-deserialize-binary-tree/
 public class hw6 {
@@ -11,4 +14,62 @@ public class hw6 {
             val = v;
         }
     }
+    // 队列的实现
+    TreeNode[] queue = new TreeNode[10001];
+    int l =0, r = 0;
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) {
+            return sb.toString();
+        }
+        queue[r++] = root;
+        sb.append(root.val + ",");
+        while (l < r) {
+            // 注意这里的写法，不能出队列在结算，而是进去九结算，因为空不进去，没办法
+            TreeNode cur = queue[l++];
+            if (cur.left != null) {
+                sb.append(cur.left.val + ",");
+                queue[r++] = cur.left;
+            } else {
+                sb.append("#,");
+            }
+            if (cur.right != null) {
+                sb.append(cur.right.val + ",");
+                queue[r++] = cur.right;
+            } else {
+                sb.append("#,");
+            }
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.equals("")) {
+            return null;
+        }
+        String[] dat = data.split(",");
+        l = r = 0;
+        int index = 0;
+        TreeNode root = generate(dat[index++]);
+        queue[r++] = root;
+        while (l < r) {
+            TreeNode cur = queue[l++];
+            cur.left = generate(dat[index++]);
+            cur.right = generate(dat[index++]);
+            if (cur.left != null) {
+                queue[r++] = cur.left;
+            }
+            if (cur.right != null) {
+                queue[r++] = cur.right;
+            }
+        }
+        return root;
+    }
+    // 生成一个结点
+    private TreeNode generate(String val) {
+        return val.equals("#") ? null : new TreeNode(Integer.valueOf(val));
+    }
+
 }
--- src/main/java/day036/hw7.java
+++ src/main/java/day036/hw7.java
@@ -1,4 +1,43 @@
 package day036;
 
+import java.util.HashMap;
+
+// 利用先序与中序遍历序列构造二叉树
+// 测试链接 : https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 public class hw7 {
+    // 不提交这个类
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int v) {
+            val = v;
+        }
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        int n = preorder.length;
+        TreeNode root = f(preorder, 0, n - 1, inorder, 0, n - 1, map);
+        return root;
+    }
+
+
+    public TreeNode f(int[] preorder, int pl, int pr, int[] inorder, int il, int ir, HashMap<Integer, Integer> map) {
+        if(pl > pr) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[pl]);
+        if (pl == pr) {
+            return root;
+        }
+        int k = map.get(preorder[pl]);
+        root.left = f(preorder, pl + 1, pl + k - il, inorder, il, k - 1, map);
+        root.right = f(preorder, pl + k - il, pr, inorder, k + 1, ir, map);
+        return root;
+    }
+
 }
